83fc2952becb28bc21211ab85356ca36b3f244bb
plz972k1 <84142100+plz972k1@users.noreply.github.com>
2023-11-14 23:58:59 +0700

    update

--- multi_vendor_user/lib/Model/categories.dart
+++ multi_vendor_user/lib/Model/categories.dart
@@ -3,6 +3,11 @@ class CategoriesModel {
 
   final String category;
   final String image;
+  final String houseNumber;
+  final String closestbusStop;
+  final String id;
+  final String? uid;
+  
   CategoriesModel({
     required this.category,
     this.uid,
--- multi_vendor_user/lib/Model/history.dart
+++ multi_vendor_user/lib/Model/history.dart
@@ -1,23 +1,16 @@
 class HistoryModel {
   final String message;
   final String amount;
-  final String paymentSystem;
-  final String? uid;
-  final String timeCreated;
 
   HistoryModel({
     required this.message,
     required this.amount,
-    required this.paymentSystem,
-    required this.timeCreated,
-    this.uid,
+
   });
 
   HistoryModel.fromMap(Map<String, dynamic> data, this.uid)
       : message = data['message'],
         amount = data['amount'],
-        paymentSystem = data['paymentSystem'],
-        timeCreated = data['timeCreated'];
 
   Map<String, dynamic> toMap() {
     return {
