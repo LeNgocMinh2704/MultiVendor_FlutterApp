85bd1ea7cccc58436c9906b5ea5781f280558d7c
Minh Le <20521600@gm.uit.edu.vn>
2023-11-17 21:47:09 +0700

    Fix bug in buying goods

--- multi_vendor_client/lib/main.dart
+++ multi_vendor_client/lib/main.dart
@@ -113,6 +113,10 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ],
         ),
       ),
