93ae57270d81daebe43b062caadb126f70f09729
Minh Le <20521600@gm.uit.edu.vn>
2023-12-17 21:47:47 +0700

    Fix bug

--- multi_vendor_client/lib/main.dart
+++ multi_vendor_client/lib/main.dart
@@ -113,10 +113,6 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
           ],
         ),
       ),
